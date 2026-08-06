d759908acd83d3f02d74c868c656b4144f1dd1c1
Leonardo Gomes <leonardo.tgomes@ufn.edu.br>
2026-08-06 10:36:05 -0300

    Add main class to execute ListaController

---
+++ Codigos/Aula 4/Java/scr/main.java
@@ -0,0 +1,10 @@
+package projeto;
+
+import projeto.controller.ListaController;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaController controller = new ListaController();
+        controller.executarProcessamento();
+    }
+}
