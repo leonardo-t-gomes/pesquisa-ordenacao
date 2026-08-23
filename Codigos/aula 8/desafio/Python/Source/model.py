e45a91cb315110b9ce6e79d4ea54e3613ca39286
Leonardo Gomes <leonardo.tgomes@ufn.edu.br>
2026-08-23 19:18:05 -0300

    Create model.py

---
+++ Codigos/aula 8/desafio/Python/Source/model.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from datetime import date
+
+@dataclass
+class model:
+    minha_lista = [
+    model(id=10, data=date(2026, 8, 25)),
+    model(id=5,  data=date(2026, 8, 23)),  
+    model(id=20, data=date(2026, 8, 23))  
+]
